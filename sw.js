79215d48c907f5b07ddc4c1b818f52b0e2daba3b
frameRate(0) <frameRateZero@gmail.com>
2026-02-26 18:48:19 -0800

    Update cache version to theta-wave-v3.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'theta-wave-v3.1';
+const CACHE = 'theta-wave-v3.2';
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./manifest.json'])));
